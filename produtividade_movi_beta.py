572797bddb8f15bb3f4753b2d3f040ecb3e10721
DigoRM <82246925+DigoRM@users.noreply.github.com>
2021-09-30 17:10:32 -0300

    Update produtividade_movi_beta.py

--- produtividade_movi_beta.py
+++ produtividade_movi_beta.py
@@ -488,7 +488,7 @@ Operador_Atendimentos_Data ['Meta Atendimentos'] = Meta_Atendimentos_Diarios
 Agentes_Analisados = len(Analise_Desempenho)
 Operador_Atendimentos_Data['Meta Atendimentos Equipe'] = Meta_Atendimentos_Diarios*Agentes_Analisados
 
-plot = go.Figure(data=[go.Bar(name= 'Atendimentos',x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Atendimentos']),go.Line(name="Média Atendimentos Individual",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Média Atendimentos Equipe']), go.Line(name="Meta Atendimentos Individual",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Meta Atendimentos']), go.Line(name="Linha de Tendência",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Atendimentos']),go.Line(name="Potencial da Equipe",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Meta Atendimentos Equipe'])])
+plot = go.Figure(data=[go.Bar(name= 'Atendimentos',x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Atendimentos']),go.Line(name="Média Atendimentos Individual",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Média Atendimentos Equipe']), go.Line(name="Meta Atendimentos Individual",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Meta Atendimentos']), go.Line(name="Linha de Tendência",x=Operador_Atendimentos_Data.index,y=Operador_Atendimentos_Data['Atendimentos'])])
 plot.update_layout(title='Atendimentos', height=500)
 
 st.plotly_chart(plot, use_container_width=True)
