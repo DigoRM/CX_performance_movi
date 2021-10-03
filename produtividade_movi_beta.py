24860884b336a27bfe94b805ee6942b0cc6ae717
DigoRM <82246925+DigoRM@users.noreply.github.com>
2021-10-03 19:42:12 -0300

    Update produtividade_movi_beta.py

--- produtividade_movi_beta.py
+++ produtividade_movi_beta.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Produtividade CX", page_icon=":bar_chart:", layou
 st.title('Analise de Desempenho CX - MOVI')
 st.markdown('Inicialmente farei um tratamento nos dados... se aparecer o dataframe, pode prosseguir!')
 
-input_Dias_Analisados = st.number_input('Quantos dias úteis vou analisar?','')
+input_Dias_Analisados = st.number_input('Quantos dias úteis vou analisar?',min_value=0,max_value=30,value=1,step=1)
 if input_Dias_Analisados is not None:
 
 
