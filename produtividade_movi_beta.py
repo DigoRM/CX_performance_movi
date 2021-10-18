6a390bc172b228c1f2acf5482c46ba6b49ad5f4a
DigoRM <82246925+DigoRM@users.noreply.github.com>
2021-10-18 11:40:22 -0300

    Update produtividade_movi_beta.py

--- produtividade_movi_beta.py
+++ produtividade_movi_beta.py
@@ -496,6 +496,28 @@ if input_Dias_Analisados is not None:
 		Mapeando_Parceiros = Mapeando_Parceiros.drop(columns=['Ticket','Ação nº','Minutos Trabalhados'])
 		Ranking_Parceiros = Mapeando_Parceiros.sort_values('Atendimentos',ascending=False)
 		st.dataframe(Ranking_Parceiros)
+		
+		# Aqui o download Ranking Produtividade
+
+		def to_excel(dataframe):
+		    output = BytesIO()
+		    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+		    dataframe.to_excel(writer, sheet_name='Sheet1')
+		    writer.save()
+		    processed_data = output.getvalue()
+		    return processed_data
+
+		def get_table_download_link(dataframe):
+		    """Generates a link allowing the data in a given panda dataframe to be downloaded
+		    in:  dataframe
+		    out: href string
+		    """
+		    val = to_excel(dataframe)
+		    b64 = base64.b64encode(val)  # val looks like b'...'
+		    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Ranking Parceiros!</a>' # decode b'abc' => abc
+
+		dataframe = Ranking_Parceiros # your dataframe
+		st.markdown(get_table_download_link(dataframe), unsafe_allow_html=True)
 
 	with column6:
 		# Grafico
